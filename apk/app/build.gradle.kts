2576f997400157fc042ee72edd5e6793758ba634
Le0xFF <le0xff@noreply.codeberg.org>
2026-07-31 12:24:08 +0200

    feat(reordering): use Reorderable lib to properly manually reorder list items

--- apk/app/build.gradle.kts
+++ apk/app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.pebblekit2.client)
+    implementation(libs.reorderable)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
