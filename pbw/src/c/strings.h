f1c59967b9df329ee207cfb7753d6ae7b46ca6dc
Le0xFF <le0xff@noreply.codeberg.org>
2026-08-02 15:36:14 +0200

    update(ui): update watchapp ui
    * Move app text and icon to center of the screen
    * Change labels with icons: https://github.com/pebble-dev/iconography

--- pbw/src/c/strings.h
+++ pbw/src/c/strings.h
@@ -1,9 +1,6 @@
 #ifndef PLAUNCHER_STRINGS_H
 #define PLAUNCHER_STRINGS_H
 
-#define STR_LABEL_UP "Up"
-#define STR_LABEL_DOWN "Down"
-#define STR_LABEL_LAUNCH "Launch"
 #define STR_LOADING_MESSAGE "Loading..."
 
 const char* str_empty_message(void);
